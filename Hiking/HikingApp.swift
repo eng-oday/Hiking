3139be1bd170ec8110d0a1b509c4303927275095
eng-oday <odaydieg@gmail.com>
2023-07-11 14:40:35 +0300

    Initial Commit

---
+++ Hiking/HikingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikingApp.swift
+//  Hiking
+//
+//  Created by 3rabApp-oday on 11/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct HikingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
